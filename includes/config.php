5ccee8ca183e19e8c65348d0125d101596695445
Ailyn Hernández Hermosillo <22151242@aguascalientes.tecnm.mx>
2026-09-03 20:15:25 -0600

    Nuevos archivos

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'LYM');
+define('DB_NAME', 'lym');
 
 
 // =====================================================
